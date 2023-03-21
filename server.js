936c3cc4253997af0548d3e84e376fba2d4aa2b2
Babatunde Koiki <koikibabatunde14@gmail.com>
2023-03-21 11:17:50 +0100

    setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req.headers)
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
